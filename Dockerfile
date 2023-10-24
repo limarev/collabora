6469279feaa27181072e0463f3bb662ab7f77785
limarev <limarev@ians.aero>
2023-10-24 14:24:50 +0300

    entrypoint is added

--- Dockerfile
+++ Dockerfile
@@ -4,3 +4,10 @@ RUN useradd -ms /bin/bash user
 
 USER user
 WORKDIR /home/user
+
+COPY entrypoint.sh /home/user
+
+ENV ANDROID_SDK_PATH=/opt/android_sdk/
+ENV NDK_PATH=${ANDROID_SDK_PATH}/ndk-bundle
+
+ENTRYPOINT [ "./entrypoint.sh" ]
---
+++ entrypoint.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+set -x
+
+
+function build {
+    echo \
+    "--with-android-package-name=com.collabora.for.gerrit
+    --with-external-tar=/home/user/build/external/tarballs
+    --disable-fetch-external
+    --with-android-ndk=${NDK_PATH}
+    --with-android-sdk=${ANDROID_SDK_PATH}
+    --with-distro=$1
+    --disable-ccache" > autogen.input
+    make distclean
+    ./autogen.sh && make
+}
+
+cd /home/user/build
+
+case $1 in
+    "fetch")
+        make distclean
+        ./autogen.sh --with-all-tarballs && make fetch
+        ;;
+    "CPAndroid")
+        build "CPAndroid"
+        ;;
+    "CPAndroidAarch64")
+        build "CPAndroidAarch64"
+        ;;
+    "CPAndroidX86")
+        build "CPAndroidX86"
+        ;;
+    "CPAndroidX86_64")
+        build "CPAndroidX86_64"
+        ;;
+    *)
+        echo "Available options: fetch CPAndroid CPAndroidAarch64 CPAndroidX86 CPAndroidX86_64"
+esac
